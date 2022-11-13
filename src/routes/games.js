44a4dbbcf19ed49e6dff3c10d746371b7c2bcd60
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2022-11-13 17:45:25 -0500

    Add bodyParser to show requirement sent

--- src/routes/games.js
+++ src/routes/games.js
@@ -1,12 +1,20 @@
 const { Router } = require('express');
 const router = Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
 
 const games = require('../sample.json');
 
-router.get('/games', (req, res) => {
+router.get('/games', jsonParser, (req, res) => {
     // res.send("Hello from games route");
     const data = games;
     res.json(data);
+});
+
+router.post('/games', jsonParser, (req, res) => {
+    console.log(req.body);
+    res.send("Recibido");
 });
 
 module.exports = router;
