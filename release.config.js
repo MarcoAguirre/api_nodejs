728f2ab7901e6d49abf2dc4126c440295b9d230b
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2023-02-28 12:30:50 -0500

    feat: test actions v12

---
+++ release.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    branches: ['test/actions_tag'],
+    tagFormat: "v${version}-stg",
+    plugins: [
+        ["@semantic-release/commit-analyzer", {
+            "preset": "angular",
+            "releaseRules": [
+                { "type": "docs", "release": "patch" },
+                { "type": "refactor", "release": "patch" },
+                { "type": "style", "release": "patch" }
+            ]
+        }],
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        '@semantic-release/github',
+        ["@semantic-release/git", {
+            "assets": ["CHANGELOG.md"],
+            "message": "chore(release): ${nextRelease.version} [skip ci]"
+        }]
+    ],
+};
