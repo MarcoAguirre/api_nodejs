618af37669b1523fa4e1942edb2281f54b1fade4
MarcoAguirre <42116904+MarcoAguirre@users.noreply.github.com>
2023-03-02 12:53:17 -0500

    feat: add release config file

--- release.config.js
+++ release.config.js
@@ -1,22 +1,56 @@
 module.exports = {
-    branches: ['test/release_tag'],
-    tagFormat: "v${version}-stage",
+    branches: ["main"],
+    tagFormat: "v${version}-stg",
     plugins: [
-        ["@semantic-release/commit-analyzer", {
-            "preset": "angular",
-            "releaseRules": [
-                { "type": "docs", "release": "patch" },
-                { "type": "refactor", "release": "patch" },
-                { "type": "style", "release": "patch" },
-                { "type": "release", "release": "major" }
-            ]
-        }],
-        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                preset: "angular",
+                releaseRules: [
+                    { type: "docs", release: "patch" },
+                    { type: "test", release: "patch" },
+                    { type: "build", release: "patch" },
+                    { type: "ci", release: "patch" },
+                    { type: "refactor", release: "patch" },
+                    { type: "style", release: "patch" },
+                    { type: "release", release: "major" },
+                ],
+                parserOpts: {
+                    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+                },
+            },
+        ],
+        [
+            "@semantic-release/release-notes-generator",
+            {
+                parserOpts: {
+                    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+                },
+                presetConfig: [
+                    { type: "feat", section: "Features" },
+                    { type: "fix", release: "Bug Fixes" },
+                    { type: "docs", section: "Documentation", hidden: false },
+                    { type: "test", section: "Test", hidden: true },
+                    { type: "build", section: "Build", hidden: true },
+                    { type: "ci", section: "CI/CD", hidden: false },
+                    { type: "refactor", section: "Code Refactors", hidden: false },
+                    { type: "style", section: "Styles", hidden: true },
+                ],
+            },
+        ],
         "@semantic-release/changelog",
-        '@semantic-release/github',
-        ["@semantic-release/git", {
-            "assets": ["CHANGELOG.md"],
-            "message": "chore(release): ${nextRelease.version} [skip ci]"
-        }]
+        [
+            "@semantic-release/github",
+            {
+                releasedLabels: false,
+            },
+        ],
+        [
+            "@semantic-release/git",
+            {
+                assets: ["CHANGELOG.md", "ui/**", "docs/**", "api/**"],
+                message: "chore(release): ${nextRelease.version}",
+            },
+        ],
     ],
 };
